46c1d05207f725b0d5e1e083357ae978af41ef8b
rfhfntkm <rfhfntkm04@yandex.ru>
2014-10-18 21:40:52 +0300

    Add Firefox extension

---
+++ Firefox/chrome/content/putin-to-huylo.js
@@ -0,0 +1,88 @@
+(function() {
+
+    function walk(node) 
+    {
+        // I stole this function from here:
+        // http://is.gd/mwZp7E
+    
+        var child, next;
+    
+        switch ( node.nodeType )  
+        {
+            case 1:  // Element
+            case 9:  // Document
+            case 11: // Document fragment
+                child = node.firstChild;
+                while ( child ) 
+                {
+                    next = child.nextSibling;
+                    walk(child);
+                    child = next;
+                }
+                break;
+    
+            case 3: // Text node
+                handleText(node);
+                break;
+        }
+    }
+    
+    function handleText(textNode)
+    {
+        var v = textNode.nodeValue;
+    
+        v = v.replace(/\bPutin\b/g, "Huylo");
+        v = v.replace(/\bputin\b/g, "huylo");
+
+        v = v.replace(/\bPutina\b/g, "Huylo");
+        v = v.replace(/\bputina\b/g, "huylo");
+
+        v = v.replace(/Путина/g, "Хуйла");
+        v = v.replace(/путина/g, "хуйла");
+   
+        v = v.replace(/Путину/g, "Хуйлу");
+        v = v.replace(/путину/g, "хуйлу");
+    
+        v = v.replace(/Путиным/g, "Хуйлом");
+        v = v.replace(/путиным/g, "хуйлом");
+
+        v = v.replace(/Путине/g, "Хуйле");
+        v = v.replace(/путине/g, "хуйле");
+
+        v = v.replace(/Путины/g, "Хуйловы");
+        v = v.replace(/путины/g, "хуйловы");
+
+        v = v.replace(/Путиных/g, "Хуйловых");
+        v = v.replace(/путиных/g, "хуйловых");
+
+        v = v.replace(/Пу́тин/g, "Хуйло́");
+        v = v.replace(/пу́тин/g, "хуйло́");
+
+        v = v.replace(/Путин/g, "Хуйло");
+        v = v.replace(/путин/g, "хуйло");
+    
+    
+        v = v.replace(/Путіна/g, "Хуйла");
+        v = v.replace(/путіна/g, "хуйла");
+    
+        v = v.replace(/Путін/g, "Хуйло");
+        v = v.replace(/путін/g, "хуйло");
+    
+        v = v.replace(/Пу́тін/g, "Хуйло́")
+        v = v.replace(/пу́тін/g, "хуйло́")
+    
+        textNode.nodeValue = v;
+    }
+
+    function windowLoadHandler()
+    {
+        // Dear Mozilla: I hate you for making me do this.
+        window.removeEventListener('load', windowLoadHandler);
+
+        document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(e) {
+            walk(e.originalTarget.body);
+        });
+    }
+
+    window.addEventListener('load', windowLoadHandler);
+}());
